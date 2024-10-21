a781111aa786363e3643ffb76eace319dfe9ac07
aein <mananniaein1234@gmail.com>
2024-10-21 09:13:04 +0330

    Create Axios

---
+++ src/components/hook.tsx
@@ -0,0 +1,10 @@
+// import { useState, useEffect } from "react";
+
+// type FetchType<T> = {
+//   data: T;
+//   url:string
+// };
+
+// export const useFetch = <T,> ({url,data}:FetchType<T>) =>{
+
+// }
